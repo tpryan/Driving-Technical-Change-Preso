3b793a309660f3a869e45032c300e969a6b62113
Terrence Ryan <terry@terrenceryan.com>
2013-06-11 12:09:37 -0400

    Tweaked a whole bunch of images and content.

--- js/dtc.js
+++ js/dtc.js
@@ -1,12 +1,29 @@
 /* global Reveal */
 function blurBackgroundImage(){
 	var backgroundholder = document.querySelector(".state-background");
-	backgroundholder.style.webkitFilter = "blur(10px)";
+	backgroundholder.style.webkitFilter = "blur(7px)";
 }
 
 function deBlurBackgroundImage(){
 	var backgroundholder = document.querySelector(".state-background");
 	backgroundholder.style.webkitFilter = "";
+}
+
+function whichTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    };
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
 }
 
 Reveal.addEventListener( 'fragmentshown', function( event ) {
@@ -21,6 +38,8 @@ Reveal.addEventListener( 'fragmenthidden', function( event ) {
 	}
 } );
 
-Reveal.addEventListener( 'slidechanged', function( event ) {
-		deBlurBackgroundImage();
+Reveal.addEventListener( 'ready', function() {
+	var transitionEnd = whichTransitionEvent();
+	document.querySelector('.state-background').addEventListener(transitionEnd, deBlurBackgroundImage, false);
+
 } );
